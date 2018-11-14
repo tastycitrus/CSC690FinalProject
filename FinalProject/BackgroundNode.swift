e75ed74224efba728083181a3529aed4a76f1e4f
Diana Luu <Diana@Jamess-MacBook-Pro.local>
2018-11-13 23:01:35 -0800

    setting up background node and player position

---
+++ FinalProject/BackgroundNode.swift
@@ -0,0 +1,21 @@
+//
+//  BackgroundNode.swift
+//  FinalProject
+//
+//  Created by Diana Luu on 11/13/18.
+//  Copyright © 2018 Diana Luu. All rights reserved.
+//
+
+import SpriteKit
+
+public class BackgroundNode: SKNode {
+    
+    public func setup(size: CGSize) {
+        let yPos: CGFloat = size.height * 0.1
+        let startPoint = CGPoint(x: 0, y: yPos)
+        let endPoint = CGPoint(x: size.width, y: yPos)
+        physicsBody = SKPhysicsBody(edgeFrom: startPoint, to: endPoint)
+        physicsBody?.restitution = 0.3
+    }
+    
+}
